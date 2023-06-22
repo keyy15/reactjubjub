6ce75312ced2f34ed2f816a65897a53a7997117c
keyy15 <smeyhem01@gmail.com>
2023-06-22 15:48:23 +0700

    Home Page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,20 +14,20 @@ function Navbar() {
   }
 
   return (
-    <div className='navbar w-full min-h-ful flex justify-between max-sm:p-2'>
+    <div className='navbar w-full min-h-ful flex justify-betwe bg-white max-sm:p-2'>
       <div className={`logo w-[50%] flex items-center justify-center ${showMenu ? 'max-sm:block' : 'max-sm:w-0'}`}>
-        <img src={Logo} alt='Logo' className='w-[20%] p-2 max-sm:w-[32%] max-sm:ml-20' />
+        <img src={Logo} alt='Logo' className='w-[14%] p-2 max-sm:w-[32%] max-sm:ml-20' />
       </div>
-      <div className={`menu w-[50%] flex items-center justify-center ${showMenu ? 'max-sm:hidden' : 'max-sm:text-[12px]'} `}>
-        <NavLink to="/" className='menu-item mr-4 text-gray-400 hover:text-white' activeClassName='active' >Home</NavLink>
-        <NavLink to="/product" className='menu-item mr-4 text-gray-400 hover:text-white' activeClassName='active'>Product</NavLink>
-        <NavLink to="/update" className='menu-item mr-4 text-gray-400 hover:text-white' activeClassName='active'>Update</NavLink>
-        <NavLink to="/about" className='menu-item mr-4 text-gray-400 hover:text-white' activeClassName='active'>About</NavLink>
-        <NavLink to="/login" className='menu-item mr-4 text-gray-400 hover:text-white' activeClassName='active'>Login</NavLink>
+      <div className={`menu w-[50%] font-bold text-black flex items-center justify-center ${showMenu ? 'max-sm:hidden' : 'max-sm:text-[12px]'} `}>
+        <NavLink to="/" className='menu-item mr-4 hover:text-[#e11d48]' activeClassName='active' >Home</NavLink>
+        <NavLink to="/product" className='menu-item mr-4 hover:text-[#e11d48]' activeClassName='active'>Product</NavLink>
+        <NavLink to="/update" className='menu-item mr-4 hover:text-[#e11d48]' activeClassName='active'>Update</NavLink>
+        <NavLink to="/about" className='menu-item mr-4 hover:text-[#e11d48]' activeClassName='active'>About</NavLink>
+        <NavLink to="/login" className='menu-item mr-4 hover:text-[#e11d48]' activeClassName='active'>Login</NavLink>
         {/* <div className='menu-item md:hidden'>
               <input type='text' placeholder='Search' className='px-2 py-1 border border-gray-300 rounded-2xl focus:outline-none focus:ring-1 focus:ring-blue-500' />
               <button className='ml-2 bg-transparent border-none focus:outline-none'>
-                <IoSearch className='text-gray-400 hover:text-white'/>
+                <IoSearch className=' hover:text-white'/>
               </button>
             </div> */}
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,21 @@
 import React from 'react'
-import Bg from '../assets/bg.jpg'
+import BackImg from '../assets/Bg.avif'
+import Smey from '../assets/sney.png'
 
 function Home() {
   return (
-    <div className='w-full min-h-ful flex justify-between'>
-      <div className='w-[40%] flex items-center justify-center'>
-        <div className='w-full flex flex-col items-stretch pl-8'>
-          <h1 className='text-[48px] text-text-custom' style={{ '--tw-text-opacity': '1', color: 'rgb(34, 99, 133)' }}>Back / Front-End</h1>
-          <span className='text-[28px] '>Landing Pages</span>
-          <p className='mt-8 '>Lorem ipsum dolor sit amet, consecteuture adipiscing elit, sed diam nonumoomy.</p>
+    <div className='w-full min-h-full bg-black relative -z-10'>
+      <img src={BackImg} alt={BackImg} className='w-[100%] h-[600px]' />
+      <div className='absolute top-1/2 left-1/2 transform translate-x-[-50%] translate-y-[-50%] flex items-center justify-between bg-red-950'>
+        <div className='title w-[50%] h-full'>
+          <h1 className='text-black text-[50px]'>Hi There,</h1>
+          <h1 className='text-black'>I'm Raksmey Hem</h1>
+          <p className='text-black'>I Am Into Web Developer</p>
+          <button> About Me  </button>
         </div>
-      </div>
-      <div className='w-[60%] rounded'>
-        <img src={Bg} alt={Bg} className='rounded'/>
+        <div className='img-box w-[50%] h-full'>
+          <img src={Smey} alt={Smey}/>
+        </div>
       </div>
     </div>
   )
