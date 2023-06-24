99db3cb758ed729291fa2790d8c83161a6f5dc5a
keyy15 <smeyhem01@gmail.com>
2023-06-24 12:10:03 +0700

    Mobile responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,47 +6,53 @@ import {BsLinkedin, BsGithub, BsTwitter, BsFacebook, BsTelegram} from 'react-ico
 
 function Home() {
   return (
-    <div className='w-full min-h-ful relative -z-10'>
-      <img src={BackImg} alt={BackImg} className='w-[100%] h-[600px] opacity-50' />
-      <div className='w-full absolute top-1/2 left-1/2 transform translate-x-[-50%] translate-y-[-50%] flex items-center justify-between '>
-          <div className='title p-[100px] max-sm:w-[50%] max-sm:p-4'>
-          <h1 className='text-[#000000] text-[40px] font-bold mt-[8px] max-sm:mt-[8px]'>Hi There,</h1>
-          <h1 className='text-[#000000] text-[40px] font-normal mt-[8px] max-sm:text-[18px] max-sm:mt-[8px]'>I am <span className='text-4xl font-extrabold text-blue-600 drop-shadow-lg max-sm:mt-[8px] max-sm:font-bold max-sm:text-[23px]'>Hem Raksmey</span></h1>
-          <p className='text-[#000000] text-[20px] font-medium mt-[8px] max-sm:text-[18px] max-sm:mt-[8px]'>I Am Into <span className='text-5xl font-extrabold text-blue-600 drop-shadow-lg max-sm:mt-[8px] max-sm:text-[29px]'>Web Developer ||</span></p>
-          <button className='btn-about mt-[12px] max-sm:mt-[18px]'>About Me</button>
-          <div className='social-media mt-[24px]'>
-            <ul className='space-x-4 max-sm:flex max-sm:space-x-1'>
-              <li className='bg-slate-400'>
-                <a href='https://www.youtube.com/watch?v=akkYrXYSo28'>
-                  <BsLinkedin className='text-4xl bg-indigo-500 rounded-md max-sm:text-[32px]' />
-                </a>
-              </li>
-              <li>
-                <a href='https://www.youtube.com/watch?v=akkYrXYSo28'>
-                  <BsGithub className='text-4xl bg-indigo-500 rounded-md max-sm:text-[32px]' />
-                </a>
-              </li>
-              <li>
-                <a href='https://www.youtube.com/watch?v=akkYrXYSo28'>
-                  <BsTwitter className='text-4xl bg-indigo-500 rounded-md max-sm:text-[32px]' />
-                </a>
-              </li>
-              <li>
-                <a href='https://www.youtube.com/watch?v=akkYrXYSo28'>
-                  <BsFacebook className='text-4xl bg-indigo-500 rounded-md max-sm:text-[32px]' />
-                </a>
-              </li>
-              <li>
-                <a href='https://www.youtube.com/watch?v=akkYrXYSo28'>
-                  <BsTelegram className='text-4xl bg-indigo-500 rounded-md max-sm:text-[32px]' />
-                </a>
-              </li>
-            </ul>
+    <div>
+      <div className='w-full min-h-ful relative -z-10'>
+        <img src={BackImg} alt={BackImg} className='w-[100%] h-[688px] opacity-50 max-sm:h-[688px]' />
+        <div className='w-full flex items-center justify-around absolute top-1/2 left-1/2 transform translate-x-[-50%] translate-y-[-50%] max-sm:flex max-sm:flex-col-reverse max-sm:h-[75%]'>
+            <div className='title p-[100px] max-sm:w-full max-sm:p-6 '>
+            <h1 className='text-[#000000] text-[40px] font-bold mt-[8px] max-sm:mt-[8px] max-sm:text-[28px]'>Hi There,</h1>
+            <h1 className='text-[#000000] text-[40px] font-normal mt-[8px] max-sm:text-[18px] max-sm:mt-[8px]'>I am <span className='text-4xl font-extrabold text-blue-600 drop-shadow-lg max-sm:mt-[8px] max-sm:font-bold max-sm:text-[23px]'>Hem Raksmey</span></h1>
+            <p className='text-[#000000] text-[20px] font-medium mt-[8px] max-sm:text-[18px] max-sm:mt-[8px]'>I Am Into <span className='text-5xl font-extrabold text-blue-600 drop-shadow-lg max-sm:mt-[8px] max-sm:text-[29px]'>Web Developer</span></p>
+            <button className='btn-about mt-[12px] max-sm:mt-[18px]'>About Me</button>
+            <div className='social-media mt-[24px]'>
+              <ul className='space-x-4 max-sm:flex max-sm:space-x-1'>
+                <li className='bg-slate-400'>
+                  <a href='https://www.youtube.com/watch?v=akkYrXYSo28'>
+                    <BsLinkedin className='text-4xl bg-indigo-500 rounded-md max-sm:text-[32px]' />
+                  </a>
+                </li>
+                <li>
+                  <a href='https://www.youtube.com/watch?v=akkYrXYSo28'>
+                    <BsGithub className='text-4xl bg-indigo-500 rounded-md max-sm:text-[32px]' />
+                  </a>
+                </li>
+                <li>
+                  <a href='https://www.youtube.com/watch?v=akkYrXYSo28'>
+                    <BsTwitter className='text-4xl bg-indigo-500 rounded-md max-sm:text-[32px]' />
+                  </a>
+                </li>
+                <li>
+                  <a href='https://www.youtube.com/watch?v=akkYrXYSo28'>
+                    <BsFacebook className='text-4xl bg-indigo-500 rounded-md max-sm:text-[32px]' />
+                  </a>
+                </li>
+                <li>
+                  <a href='https://www.youtube.com/watch?v=akkYrXYSo28'>
+                    <BsTelegram className='text-4xl bg-indigo-500 rounded-md max-sm:text-[32px]' />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className='img-box w-[50%] h-full flex items-center justify-center max-sm:w-full max-sm:h-[25%]'>
+            <img src={Smey} alt={Smey} className='custom-rounded h-[400px] rounded-full max-sm:h-[240px]'/>
           </div>
         </div>
-        <div className='img-box w-[50%] h-full flex items-center justify-center '>
-          <img src={Smey} alt={Smey} className='h-[400px] bg-indigo-500 rounded-full max-sm:h-[200px]'/>
-        </div>
+      </div>
+      <div className='w-full h-[680px] bg-gray-600 flex items-center justify-around'>
+        <div className='content-left'>Left</div>
+        <div className='content-right'>Right</div>
       </div>
     </div>
   )
